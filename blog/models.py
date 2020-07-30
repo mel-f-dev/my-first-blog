0711ad933d835085e00c4fbf0feba2d19a612fb7
amelie <mel.f.dev@gmail.com>
2020-07-30 19:58:35 +0900

    Added publish&remove btn

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,10 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
@@ -17,3 +19,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+def publish(self):
+    self.published_date = timezone.now()
+    self.save()
